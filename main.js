93382b4419b181f608c13c631d696624378866a5
Louise Wong <lw17122@bristol.ac.uk>
2021-11-24 16:38:31 +0000

    added comment

--- main.js
+++ main.js
@@ -3,28 +3,26 @@ const submitBtn = document.querySelector(".input-box__submitBtn");
 const resetBtn = document.querySelector("#reset-btn");
 const output = document.querySelector(".task__bar");
 
-// store the 
+// store the task input into an array
 let tasks = [];
 
+// Once submitted, input value will be stored in the array
 const recieveTask = () =>{
   let inputTask = input.value;
   tasks.push(inputTask);
   console.log(tasks)
 }
 
+// Print Task onto the HTML in div
 const printTask = () =>{
-  // for (let i = 0; i < tasks.length; i++){
-  // console.log(tasks[i])
-    //return `<div>${output.innerHTML = tasks[i]}</div>`
-  
   output.innerHTML += `<div>
   <input type="checkbox"/>${output.innerHTML = tasks}
   <i class="fas fa-pencil-alt"></i>
   </div>
   `
+  // After printing one, clear the array so the second task 
+  // won't clash with the first
   tasks=[];
-  
-  // }
     
 
 }
@@ -33,6 +31,6 @@ const printTask = () =>{
 
 
 
-
+// Trigger event
 submitBtn.addEventListener("click",recieveTask)
 submitBtn.addEventListener("click",printTask)
